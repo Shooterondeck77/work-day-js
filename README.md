33f606451c253fbc328b1c761f12db33a8614de2
Shooterondeck77 <timothytaffur@gmail.com>
2023-08-18 19:19:18 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # work-day-js
+
+# Description 
+
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+
+## Technologies Used
+HTML
+CSS
+JS
+
+
+## Deployed website
+
+https://shooterondeck77.github.io/code-quiz/
+
+## Installation
+N/A
+
+## Usage
+The user will be able to create a work schedule and save it
+
+## Credits
+Created by Timothy 
--- develop/style.js
+++ develop/style.js
@@ -1,36 +1,30 @@
-// Display current time and date 
-var dateTimeTTEL = document.getElementById('DateTimeTT')
-var today = new Date();
-console.log(today);
-dateTimeTTEL.textContent = today;
-var saveBtn = document.querySelector(".saveBtn")
-var text = $(this).siblings(".description").val();
-var time = $(this).parent().attr("id");
-if (currenttime)
-
+// Display current time and date
+const date = new Date();
+ console.log(date);
+ var current = document.querySelector(".date")
 
 
 $(document).ready(function () {
     // SaveButton click listener 
         $('saveButton').click(function() {
-         
+            var text = $(this).siblings(".description").val();
+            var time = $(this).parent().attr("id");
         localStorage.setItem(time, text);
         });
-      });
+      
     
-function timeKeeper () {
-var timeNow = moment().hour();
+function timeTracker () {
+    var currentHour = dayjs().format();
 
-}
 $(".time-stop").each (function () {
-    var blockTime = parseInt ($(this).attr("id").split("hour")[1]);
+    var blockTime = parseInt ($(this).attr("id").split("-")[1]);
 // We take the current time and add the classes for the color indicator"
     if (blockTime < timeNow) {
         $(this).removeClass ("future");
         $(this).removeClass ("present");
         $(this).addClass ("past");
     }
-    else if(blockTime === timeNow) {
+    else if(blockTime == timeNow) {
         $(this).removeClass ("future");
         $(this).removeClass ("past");
         $(this).addClass ("present");
@@ -42,18 +36,18 @@ $(".time-stop").each (function () {
     }
 
 })
-
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-
-
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-  });
+}
+    // Any user input that was saved in localStorage
+    $("#hour9.description").val(localStorage.getItem("hour9"));
+    $("#hour10.description").val(localStorage.getItem("hour10"));
+    $("#hour11.description").val(localStorage.getItem("hour11"));
+    $("#hour12.description").val(localStorage.getItem("hour12"));
+    $("#hour13.description").val(localStorage.getItem("hour13"));
+    $("#hour14.description").val(localStorage.getItem("hour14"));
+    $("#hour15.description").val(localStorage.getItem("hour15"));
+    $("#hour16.description").val(localStorage.getItem("hour16"));
+    $("#hour17.description").val(localStorage.getItem("hour17"));
+    
+    timeTracker ();
+  })
   
